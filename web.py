3c6159baeebc2efd5174800f7155be718be5065b
scl971009 <dora20823@gmail.com>
2020-06-03 16:23:19 +0800

    first commit

---
+++ web.py
@@ -0,0 +1,22 @@
+from flask import Flask, flash, redirect, render_template, request, session
+import os
+from bson.json_util import dumps
+import json
+from werkzeug import serving
+import ssl
+import hashlib
+import subprocess
+import sys
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain("server.crt", "server.key")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('a.html')
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
+    serving.run_simple("0.0.0.0", 8888, app, ssl_context=context, threaded=True)
