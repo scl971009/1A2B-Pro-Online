2329296d9f343aa6b05331e843e993c6c329d81f
kevin996231 <b05902128@ntu.edu.tw>
2020-06-07 20:04:17 +0800

    Add router

---
+++ src/components/Pages/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Login extends Component {
+  
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="py-3">Login Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Login;
