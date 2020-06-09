727ffcc3ec9fd5f3e69f784d24041165246d8e31
b05902105 <mi981027@gmail.com>
2020-06-10 02:10:22 +0800

    add login state (without connecting with backend

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 class Home extends Component {
-  
+  constructor(props){
+    super(props);
+  }
 
+  
   render() {
     return (
       <div className="container">
         <h1 className="py-3">Home</h1>
+        <h1>Status: {this.props.loggedInStatus}</h1>
       </div>
     );
   }
