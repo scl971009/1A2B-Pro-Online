44613267f89d12e3f091964477256254173e036c
pemeyolo <peterhsu7777@gmail.com>
2020-06-16 16:08:18 +0800

    add socket.io

--- web.py
+++ web.py
@@ -4,82 +4,95 @@ import json
 from werkzeug import serving
 import ssl
 from tool import control_db as db
+from flask_socketio  import SocketIO, emit
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain("server.crt", "server.key")
 
 app = Flask(__name__)
+#app.secret_key = os.urandom(12)
+
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
 def show_user_api():
-	return render_template('user_api.html')
+    return render_template('user_api.html')
+
+@socketio.on('connect_event')
+def connected_msg(msg):
+    print(msg['data'])
+    emit('server_response', {'gameid': msg['rand']})
+
+@socketio.on('CREATE_GAME')
+def connected_msg(msg):
+    print('[DEBUG] recv CREATE_GAME')
 
 @app.route('/sign_up/<string:account>/<string:password>/<string:name>')
 def sign_up(account, password, name):
-	try:
-		result, e = db().add_new_user(account, password, name)
-	except:
-		result = -1
-		e = "Unknown error"
-	return {"result": result, "account": account, "name": name, "exception": e}
+    try:
+        result, e = db().add_new_user(account, password, name)
+    except:
+        result = -1
+        e = "Unknown error"
+    return {"result": result, "account": account, "name": name, "exception": e}
 
 @app.route('/get_login/<string:account>')
 def get_login(account):
-	result, e = db().get_password(account)
-	return {"password": result, "exception": e}
+    result, e = db().get_password(account)
+    return {"password": result, "exception": e}
 
 @app.route('/get_score/<string:account>')
 def get_score(account):
-	result = db().get_score(account)
-	return result
+    result = db().get_score(account)
+    return result
 
 @app.route('/pvp/add_win/<string:account>')
 def add_win_p(account):
-	try:
-		result, e = db().add_win_and_update_score_p(account)
-	except:
-		result = -1
-		e = "Unknown error"
-	return {"result": result, "exception": e}
+    try:
+        result, e = db().add_win_and_update_score_p(account)
+    except:
+        result = -1
+        e = "Unknown error"
+    return {"result": result, "exception": e}
 
 @app.route('/pvp/add_lose/<string:account>')
 def add_lose_p(account):
-	try:
-		result, e = db().add_lose_and_update_score_p(account)
-	except:
-		result = -1
-		e = "Unknown error"
-	return {"result": result, "exception": e}
+    try:
+        result, e = db().add_lose_and_update_score_p(account)
+    except:
+        result = -1
+        e = "Unknown error"
+    return {"result": result, "exception": e}
 
 @app.route('/pve/add_win/<string:account>')
 def add_win_e(account):
-	try:
-		result, e = db().add_win_and_update_score_e(account)
-	except:
-		result = -1
-		e = "Unknown error"
-	return {"result": result, "exception": e}
+    try:
+        result, e = db().add_win_and_update_score_e(account)
+    except:
+        result = -1
+        e = "Unknown error"
+    return {"result": result, "exception": e}
 
 @app.route('/pve/add_lose/<string:account>')
 def add_lose_e(account):
-	try:
-		result, e = db().add_lose_and_update_score_e(account)
-	except:
-		result = -1
-		e = "Unknown error"
-	return {"result": result, "exception": e}
+    try:
+        result, e = db().add_lose_and_update_score_e(account)
+    except:
+        result = -1
+        e = "Unknown error"
+    return {"result": result, "exception": e}
 
 @app.route('/online/<string:account>/<path:rival>')
 def determine_rival(account, rival):
-	rival_list = []
-	if '/' in rival:
-		rival_list= rival.split('/')
-	else:
-		rival_list.append(rival)
-	result, e = db().determine_rival(account, rival_list)
-	return {"rival": result, "exception": e}
+    rival_list = []
+    if '/' in rival:
+        rival_list= rival.split('/')
+    else:
+        rival_list.append(rival)
+    result, e = db().determine_rival(account, rival_list)
+    return {"rival": result, "exception": e}
 
 if __name__ == '__main__':
-    app.secret_key = os.urandom(12)
-    serving.run_simple("127.0.0.1", 5000, app, ssl_context=context, threaded=True)
+#    serving.run_simple("127.0.0.1", 5000, app, ssl_context=context, threaded=True)
 #    serving.run_simple("140.112.30.46", 7878, app, ssl_context=context, threaded=True)
+    socketio.run(app, debug=True, host='127.0.0.1', port=5000, certfile='server.crt', keyfile='server.key')
