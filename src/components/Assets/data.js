c79aed9c81a69ed1b7bb0aebbebc3c64b76e2fb2
pemeyolo <peterhsu7777@gmail.com>
2020-06-16 18:06:35 +0800

    add socket.io

---
+++ src/components/Assets/data.js
@@ -0,0 +1,14 @@
+/*
+const devAPI = 'http://localhost:3000';
+const prodAPI = 'https://chess-game-api.herokuapp.com';
+const baseAPI = process.env.REACT_APP_ENV === 'development' ? devAPI : prodAPI;
+*/
+const baseAPI = 'https://127.0.0.1:5000';
+
+const data = {
+  api: `${baseAPI}/api`,
+  // api: prodAPI,
+  baseAPI,
+};
+
+export default data;
